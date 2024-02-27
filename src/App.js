2a1d4afe12b7dab1b242966f3f1ee0d8e22eaa4f
Abdullah <hunny7428@gmail.com>
2024-02-27 10:34:33 +0500

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import "./App.css";
+import LandingPage from "./components/LandingPage";
+import style from "./modules/App.module.css";
 function App() {
   return (
-  <>\
-  </>
+    <>
+      <div style={{height:'120vh'}}>
+        <LandingPage />
+      </div>
+    </>
   );
 }
 
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import style from "../modules/Landing.module.css";
 
 const LandingPage = () => {
-  return;
-  <div>
-    <h1>Hello</h1>
-  </div>;
+  const [height, setHeight] = useState(0);
+
+  const handleScroll = () => {
+    console.log(height);
+    setHeight(window.scrollY);
+  };
+  window.addEventListener("scroll", handleScroll);
+  return (
+    <>
+      <nav className={style.nav}>
+        <ul
+          className={style.navul}
+          style={{
+            background: height > 100 ? "black" : "transparent",
+          }}
+        >
+          <li>Home</li>
+          <li>Products</li>
+          <li>Contact</li>
+          <li>More</li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default LandingPage;
